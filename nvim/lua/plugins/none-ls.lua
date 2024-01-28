9b276bd6be66b213d120fa9fc47bc4448f91ef0b
BigLittle <biglittle@gmail.com>
2024-01-28 23:07:01 +0800

    add none-ls

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,12 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "LSP Formatting" })
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+            },
+        })
+    end,
+}
