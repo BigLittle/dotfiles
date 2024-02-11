2cebd5fcbc42d64760ce8b82c85ca469acac8cbb
BigLittle <biglittle@gmail.com>
2024-02-11 19:53:37 +0800

    Add a workaround for bad winbar showed in toggleterm.

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -2,14 +2,19 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-        require("toggleterm").setup()
-        local opts = { buffer = 0 }
-        vim.keymap.set("t", "<C-k>", [[<C-\><C-n>]], opts)
-  --      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-  --      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  --      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  --      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  --      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-  --      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+        require("toggleterm").setup({
+            on_open = function(term)
+                vim.defer_fn(function()
+                    vim.wo[term.window].winbar = ""
+                end, 0)
+            end,
+        })
+        local opts = { noremap = true }
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+        --      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        --      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        --      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        --      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+        --      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
     end,
 }
